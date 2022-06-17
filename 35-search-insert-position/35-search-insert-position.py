67fe5ad9fc52dc40cb27069ac1587a01177a401f
HYUNSOO LEE <zcarc@naver.com>
2022-06-17 23:27:06 +0900

    Time: 101 ms (10.14%), Space: 14.9 MB (6.21%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        len_nums = len(nums)
+        
+        for i in range(len_nums):
+            if nums[i] == target:
+                return i
+        
+        pointer = 0
+        for i in range(len_nums):
+            if nums[i] < target:
+                pointer = i + 1
+                
+        return pointer
+                
