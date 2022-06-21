5a26ee4481377088c1a39a36ece54dcc0926d166
HYUNSOO LEE <zcarc@naver.com>
2022-06-21 21:39:34 +0900

    Time: 81 ms (34.15%), Space: 13.8 MB (75.86%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -3,11 +3,13 @@ class Solution:
         
         roman = {'M': 1000,'D': 500 ,'C': 100,'L': 50,'X': 10,'V': 5,'I': 1}
         
-        z = 0
-        for i in range(0, len(s) - 1):
-            if roman[s[i]] < roman[s[i+1]]:
-                z -= roman[s[i]]
+        res = 0
+        for i in range(len(s) - 1):
+            if roman[s[i]] >= roman[s[i + 1]]:
+                res += roman[s[i]]
             else:
-                z += roman[s[i]]
+                res -= roman[s[i]]
                 
-        return z + roman[s[-1]]
+        return res + roman[s[-1]]
+        
+        
